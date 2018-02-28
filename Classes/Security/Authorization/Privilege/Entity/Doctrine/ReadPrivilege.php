847949d6cc96455e43c20f5be2f1061d308c17f8
Karsten Dambekalns <karsten@dambekalns.de>
2018-02-28 14:50:06 +0100

    Add a "renamed" ReadPrivilege for entities
    The existing `EntityPrivilege` actually only deals with read access,
    so to lessen confusion, let's "rename" it.

---
+++ Classes/Security/Authorization/Privilege/Entity/Doctrine/ReadPrivilege.php
@@ -0,0 +1,28 @@
+<?php
+namespace Flownative\Flow\ExtraPrivileges\Security\Authorization\Privilege\Entity\Doctrine;
+
+/*
+ * This file is part of the Flownative.Flow.ExtraPrivileges package.
+ *
+ * (c) Flownative GmbH - www.flownative.com
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Security\Authorization\Privilege\Entity\Doctrine\EntityPrivilege;
+
+/**
+ * A filter to rewrite doctrine queries according to the security policy.
+ *
+ * This is a replacement for the `Entity\Doctrine\EntityPrivilege` of Flow, the latter will
+ * be removed eventually.
+ *
+ * @Flow\Proxy(false)
+ */
+class ReadPrivilege extends EntityPrivilege
+{
+
+}
